5a5f8248e62c7cafd2169aefadde03d845ce95ec
Bayu Putra <putraarengga@gmail.com>
2023-09-15 06:56:24 +0800

    Update control_led.py

--- control_led.py
+++ control_led.py
@@ -6,8 +6,8 @@ GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 GPIO.setup(
     8, GPIO.OUT, initial=GPIO.LOW
 )  # Set pin 8 to be an output pin and set initial value to low (off)
-# while True:  # Run forever
-#     GPIO.output(8, GPIO.HIGH)  # Turn on
-#     sleep(1)  # Sleep for 1 second
-#     GPIO.output(8, GPIO.LOW)  # Turn off
-#     sleep(1)  # Sleep for 1 second
+while True:  # Run forever
+    GPIO.output(8, GPIO.HIGH)  # Turn on
+    sleep(1)  # Sleep for 1 second
+    GPIO.output(8, GPIO.LOW)  # Turn off
+    sleep(1)  # Sleep for 1 second
